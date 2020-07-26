01688001025f03b9491354f1e12ef27151a1fce1
prathimasuvarna11 <prathimasuvarna2@gmail.com>
2020-07-26 12:09:01 +0530

    log4j tutorial

---
+++ src/beta/Demo1.java
@@ -0,0 +1,18 @@
+package beta;
+
+
+import org.apache.logging.log4j.*;
+
+
+
+public class Demo1 {
+	private static Logger log=LogManager.getLogger(Demo1.class.getName());
+	public static void main(String[] args) {
+		log.debug("I am debugging");
+		log.error("Failure message");
+		log.info("Success message");
+		log.fatal("Fatal test");
+		
+	}
+
+}
